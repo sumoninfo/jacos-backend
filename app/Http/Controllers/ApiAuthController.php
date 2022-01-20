327f6854b0a215a8a809c7e97a1799dedc660c0e
Ekramul Sumon <38817030+sumoninfo@users.noreply.github.com>
2022-01-20 11:13:22 +0600

    unused code removed and updated

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -9,16 +9,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 
 class ApiAuthController extends Controller
 {
+    /**
+     * User Login
+     *
+     * @param AuthLoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(AuthLoginRequest $request)
     {
         $credentials = request(['email', 'password', 'remember_me']);
 
         if (!Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Access denied'], 403);
+            return response()->json(['message' => 'These credentials do not match our records.', 'errors' => []], 403);
         }
 
         $user = $request->user();
@@ -30,9 +35,9 @@ class ApiAuthController extends Controller
             $token->expires_at = Carbon::now()->addWeeks(1);
         }
         return response()->json([
+            'message'      => 'Successfully Login',
             'access_token' => $tokenResult->accessToken,
             'token_type'   => 'Bearer',
-            //'user' => $user,
             'role_id'      => $user->role_id,
             'expires_at'   => Carbon::parse(
                 $tokenResult->token->expires_at
@@ -40,6 +45,12 @@ class ApiAuthController extends Controller
         ]);
     }
 
+    /**
+     * User register
+     *
+     * @param AuthRegisterRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register(AuthRegisterRequest $request)
     {
         try {
@@ -55,9 +66,9 @@ class ApiAuthController extends Controller
                 $token->expires_at = Carbon::now()->addWeeks(1);
             }
             return response()->json([
+                'message'      => 'Successfully Register',
                 'access_token' => $tokenResult->accessToken,
                 'token_type'   => 'Bearer',
-                //'user' => $user,
                 'role_id'      => $user->role_id,
                 'expires_at'   => Carbon::parse(
                     $tokenResult->token->expires_at
@@ -69,6 +80,12 @@ class ApiAuthController extends Controller
         }
     }
 
+    /**
+     * User Logout
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
@@ -76,23 +93,5 @@ class ApiAuthController extends Controller
         return response()->json([
             'message' => 'Successfully logged out'
         ]);
-    }
-
-    public function logout2(Request $res)
-    {
-        if (Auth::user()) {
-            $user = Auth::user()->token();
-            $user->revoke();
-
-            return response()->json([
-                'success' => true,
-                'message' => 'Logout successfully'
-            ]);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Unable to Logout'
-            ]);
-        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,19 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
+use App\Http\Resources\AuthUserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*Route::group(['prefix' => 'v1'], function () {
-    Route::post('/login', 'UsersController@login');
-    Route::post('/register', 'UsersController@register');
-    Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
-});
-*/
-
 // API Routes
 Route::group(['prefix' => 'v1'], function () {
-    Route::post('/login', [\App\Http\Controllers\ApiAuthController::class, 'login']);
-    Route::post('/register', [\App\Http\Controllers\ApiAuthController::class, 'register']);
+    Route::post('/login', [ApiAuthController::class, 'login']);
+    Route::post('/register', [ApiAuthController::class, 'register']);
     //---------Admin Routing----------
     Route::middleware(['auth:api'])->group(function () {
         Route::get('/user', function (Request $request) {
-            return new \App\Http\Resources\AuthUserResource($request->user());
+            return new AuthUserResource($request->user());
         });
-        Route::post('/logout', [\App\Http\Controllers\ApiAuthController::class, 'logout']);
-        //dashboard
-        Route::get('/dashboard/data', [\App\Http\Controllers\DashboardController::class, 'getDashboardData']);
+        Route::post('/logout', [ApiAuthController::class, 'logout']);
     });
 });
